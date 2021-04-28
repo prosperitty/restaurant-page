d5d1d825b83531b91a8a32e88606ed8e80b2576c
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-04-28 02:28:01 -0400

    completed tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import 'bootstrap'
 import initialPageLoad from './home'
+import loadMenu from './menu'
+import loadContact from './contact';
 
-const homeTab = document.querySelector('#home-tab');
-const menuTab = document.querySelector('#menu-tab');
-const contactTab = document.querySelector('#contact-tab');
+const contentContainer = document.querySelector('#content')
 const tabs = document.querySelectorAll('.tabs');
 
 initialPageLoad();
 
-tabs.forEach((e,i) => {
-    e.addEventListener('click', (el) => {
-        console.log(el);
+tabs.forEach((e) => {
+    e.addEventListener('click', () => {
+        removeContent();
+        if(e === tabs[1]) {
+            loadMenu();
+        } else if (e === tabs[0]) {
+            initialPageLoad();
+        } else if (e === tabs[2]) {
+            loadContact();
+        }
     });
 });
 
-
+function removeContent() {
+    while (contentContainer.firstChild) {
+        contentContainer.removeChild(contentContainer.firstChild);
+      }
+}
 
 
