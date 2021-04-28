2e6fcbcaa56955fe106644e4a0dc53086504475f
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-04-28 01:23:55 -0400

    imported ihome js file and added some tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import 'bootstrap'
+import initialPageLoad from './home'
+
+const homeTab = document.querySelector('#home-tab');
+const menuTab = document.querySelector('#menu-tab');
+const contactTab = document.querySelector('#contact-tab');
+const tabs = document.querySelectorAll('.tabs');
+
+initialPageLoad();
+
+tabs.forEach((e,i) => {
+    e.addEventListener('click', (el) => {
+        console.log(el);
+    });
+});
+
+
+
 
